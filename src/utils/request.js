1d0ae73b6558bc5facaed6f37da3c62f1c3eae47
HoYoung <2807048099@qq.com>
2022-09-09 16:45:40 +0800

    登录功能开发完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
-const service = axios.create()
+import { Message } from 'element-ui'
+
+const service = axios.create({
+    baseURL: process.env.VUE_APP_BASE_API // /dev-api  /prod-api
+})
 
 // 请求拦截器
 service.interceptors.request.use()
 
 // 响应拦截器
-service.interceptors.response.use()
+service.interceptors.response.use(response => {
+    const { success, message, data } = response.data
+    if (success) {
+        return data
+    } else {
+        Message.error(message) // 提示错误消息
+        return Promise.reject(new Error(message))
+    }
+}, error => {
+    Message.error(error.message)
+    return Promise.reject(error) // 返回执行错误，让当前执行链跳出成功直接进入catch
+})
 
 export default service
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -7,7 +7,7 @@
  * @returns {Boolean}
  */
 export function isExternal(path) {
-  return /^(https?:|mailto:|tel:)/.test(path)
+    return /^(https?:|mailto:|tel:)/.test(path)
 }
 
 /**
@@ -15,6 +15,14 @@ export function isExternal(path) {
  * @returns {Boolean}
  */
 export function validUsername(str) {
-  const valid_map = ['admin', 'editor']
-  return valid_map.indexOf(str.trim()) >= 0
+    const valid_map = ['admin', 'editor']
+    return valid_map.indexOf(str.trim()) >= 0
+}
+
+/**
+ * 校验手机号
+ ***/
+export function validMobile(str) {
+    const reg = /^(13[0-9]|14[579]|15[0-35-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/
+    return reg.test(str)
 }
